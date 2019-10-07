a4bebb7cf1f3160667c289c042a9ef91d6702f12
rajsaBhai <56245611+rajsaBhai@users.noreply.github.com>
2019-10-07 02:29:40 -0500

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -7,7 +7,13 @@ public class MainClass
    public static void main(String[] args){
        
     int[] a = {10,3,5,7,1,4};
-    insertionSort(a);
+    //insertionSort(a);
+    System.out.println(findMin(a,1, 3));
+    System.out.println("UNSorted");
+    output(a);
+    System.out.println("Sorted");
+    selectionSort(a);
+    output(a);
     }
     
 //=============================================================================================================================================    
@@ -48,7 +54,46 @@ public class MainClass
 //=============================================================================================================================================    
 // 2.A
 
+  public static int findMin(int[] array, int start, int end){
+      int min = start ; // assigning the first element location to min
+      for(int i= start; i< end; i++){ // iterating through the loop
+          if(array[min]>array[i]) {// checking if the first element is bigger than any other element in the array.
+             min = i; // if yes, assigning the index of that element to min
+        }
+    }
+      return min; // returning min 
+    }
+
+//=============================================================================================================================================    
+// 2.B  
+  public static void selectionSort(int[] array){ 
+     int temp = 0; // int variable to store the variable for swapping
+     int x = 0 ; // just for my reference to check the changing values for better understanding
+     int minNum = 0; // index of the minimum value in the array
+     for(int i= 0; i< array.length; i++){ // iterating through the loop
+        minNum = findMin(array, i, array.length); // calling the function findMin and storing the value in minNum
+        if(minNum != i){ // if the new index found is different than the current one than swap the values
+            temp = array[i]; // general swapping
+            array[i] = array[minNum]; 
+            //x= array[i] = array[minNum];  
+            array[minNum] = temp;
+        }// if
+        System.out.println("After " + (i+1) +" Iterations -"); // printing out the values
+        output(array);
+        } // for
+    }// selectionSort
+
+//=============================================================================================================================================    
+    
 
 //=============================================================================================================================================    
 
+//=============================================================================================================================================    
+
+
+//=============================================================================================================================================    
+
+
+
+    
 }// MainClass
